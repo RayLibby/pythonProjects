31b847a12f3745326597630616261ec0bd7bc865
raylibby <w.ferdinand.fau@gmail.com>
2023-06-20 18:57:04 -0400

    06-20-2023

--- Day4_RockPaperScissors.py
+++ Day4_RockPaperScissors.py
@@ -28,6 +28,42 @@ scissors = '''
 # Write your code below this line 👇
 import random
 
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+# Used to ensure the users does not type something other that 0-2
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
+
+####### Debugging challenge: #########
+# Try running this code and type 5.
+# It will give you an IndexError and point to line 32 as the issue.
+# But on line 38 we are trying to prevent a crash by detecting
+# any numbers great than or equal to 3 or less than 0.
+# So what's going on?
+# Can you debug the code and fix it?
+# Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
+""" 
+# My Solution
 # Storing values in a list
 choices = ["rock", "paper", "scissors"]
 # Asking user for selection
@@ -90,8 +126,4 @@ elif userChoice == 0 and computerToNumber == 2:
 
 else:
     print("NA")
-
-
-
-
-
+"""
