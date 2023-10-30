24ba2c5e84891ac89d6509078f0a67986b2bbb61
raylibby <w.ferdinand.fau@gmail.com>
2023-10-29 23:20:35 -0400

    10/24/2023

--- Day8.py
+++ Day8.py
@@ -25,16 +25,14 @@ greet_with2(location = 'Florida',name='Waldo')
 # Write your code below this line 👇
 import math
 def paint_calc(height, width, cover):
-  cans = math.ceil((test_h*test_w)/cover)
-  print(f"You'll need {cans} cans of paint.")
-
-
+    cans = math.ceil((height*width)/cover)
+    print(f"You'll need {cans} cans of paint.")
 
 # Write your code above this line 👆
 # Define a function called paint_calc() so the code below works.   
 
 # 🚨 Don't change the code below 👇
-test_h = int(input()) # Height of wall (m)
-test_w = int(input()) # Width of wall (m)
+test_h = int(input('height: ')) # Height of wall (m)
+test_w = int(input('width: ')) # Width of wall (m)
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
